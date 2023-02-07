b8c9740c54021736718a3ff72b79800daf4d37a4
AlvaroFelipe10 <alvs.felipe@hotmail.com>
2023-02-07 10:12:44 -0300

    exercicio multa

---
+++ Codigos/MultaVeiculo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MultaVeiculo {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Tipo do veiculo (passeio, caminhao)");
+        String tipoVeiculo = entrada.nextLine();
+
+        System.out.println("Velocidade maxima da via");
+        int velocidadeMaxima = entrada.nextInt();
+
+        System.out.println("Velocidade do veiculo");
+        int velocidadeVeiculo = entrada.nextInt();
+
+        // veiculo de passeio com velocidade maior que 10%  da velocidade da via
+        // e caminhão com velocidade maior que 5% deve receber multa
+
+        /*if (tipoVeiculo.equals("passeio") && velocidadeVeiculo > velocidadeMaxima * 1.1){
+            System.out.println("Multa");
+        }else if (tipoVeiculo.equals("caminhao") && velocidadeVeiculo > velocidadeMaxima * 1.05){
+            System.out.println("Multa");
+        }  */
+
+        if((tipoVeiculo.equals("passeio") && velocidadeVeiculo > velocidadeMaxima * 1.1)
+                || (tipoVeiculo.equals("caminhao") && velocidadeVeiculo > velocidadeMaxima * 1.05)){
+                    System.out.println("Multa");
+                } else if ((tipoVeiculo.equals("passeio") && velocidadeVeiculo < velocidadeMaxima *1.1)
+                    || (tipoVeiculo.equals("caminhao") && velocidadeVeiculo < velocidadeMaxima * 1.05))
+                    {System.out.println("Velocidade permitida");
+                }
+
+    }
+}
