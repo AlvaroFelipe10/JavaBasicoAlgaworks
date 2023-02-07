d233be1446c4bd5f47f290bfe202bd71bc9ad95f
AlvaroFelipe10 <alvs.felipe@hotmail.com>
2023-02-07 14:40:15 -0300

    incremento e decremento

---
+++ Codigos/IncrementoDecremento.java
@@ -0,0 +1,16 @@
+public class IncrementoDecremento {
+    public static void main(String[]args) {
+        int idade = 10;
+        int novaIdade = ++idade;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Nova idade: " + novaIdade);
+
+        int idade1 = 11;
+        int novaIdade1 = --idade;
+
+        System.out.println("Idade: " + idade1);
+        System.out.println("Nova idade: " + novaIdade1);
+
+    }
+}
